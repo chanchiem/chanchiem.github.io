7d4937c7e6988843a93f6ba50ad622c58d4b8ded
jhlin2337 <James@nat-oitwireless-inside-vapornet100-c-31293.Princeton.EDU>
2016-03-30 13:41:20 -0400

    Penis
    Vagina

--- Main Project/PhysWiz/PhysWiz/GameScene.swift
+++ Main Project/PhysWiz/PhysWiz/GameScene.swift
@@ -7,39 +7,103 @@
 //
 
 import SpriteKit
-
 class GameScene: SKScene {
+    var stopped = true
+    var button: SKShapeNode! = nil
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        self.addChild(self.createFloor())
+        self.addChild(self.pausePlay())
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+    }
+    
+    // Creates a floor for the physics simulation
+    func createFloor() -> SKSpriteNode {
+        let floor = SKSpriteNode(color: SKColor.brownColor(), size: CGSizeMake(self.frame.size.width, 20))
         
-        self.addChild(myLabel)
+        floor.anchorPoint = CGPointMake(0, 0)
+        floor.name = "floor"
+        floor.physicsBody = SKPhysicsBody(edgeLoopFromRect: floor.frame)
+        floor.physicsBody?.dynamic = false
+        
+        return floor
+    }
+    
+    // Creates a node that will act as a pause play button for the user
+    func pausePlay() -> SKShapeNode {
+        button = SKShapeNode(circleOfRadius: 20.0)
+        button.fillColor = SKColor(red: 0.0, green: 0.0, blue: 256.0, alpha: 1)
+        button.position = CGPoint(x: 0.75 * self.size.width, y: 0.95 * self.size.height)
+        button.name = "button"
+        button.physicsBody = SKPhysicsBody(circleOfRadius: 20.0)
+        button.physicsBody?.dynamic = false
+        
+        return button
+    }
+    
+    func createBall(position: CGPoint) -> SKShapeNode {
+        let ball = SKShapeNode(circleOfRadius: 20.0)
+        let positionMark = SKShapeNode(circleOfRadius: 6.0)
+        
+        ball.fillColor = SKColor(red: CGFloat(arc4random() % 256) / 256.0, green: CGFloat(arc4random() % 256) / 256.0, blue: CGFloat(arc4random() % 256) / 256.0, alpha: 1.0)
+        ball.position = position
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 20.0)
+        ball.physicsBody?.dynamic = !stopped
+        ball.physicsBody?.restitution = 0.7
+        
+        positionMark.fillColor = SKColor.blackColor()
+        positionMark.position.y = -12
+        ball.addChild(positionMark)
+        
+        return ball
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        for touch: AnyObject in touches {
+            let location:CGPoint = touch.locationInNode(self)
+            let floor:SKNode? = self.childNodeWithName("floor")
+            if floor?.containsPoint(location) != nil {
+                self.addChild(self.createBall(location))
+            }
         }
     }
-   
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            // Gives the pause play button the ability to pause and play a scene
+            if button.containsPoint(location) {
+                for shape in self.children {
+                    if (stopped) {
+                        shape.physicsBody?.dynamic = true
+                    }
+                    else if (!stopped) {
+                        shape.physicsBody?.dynamic = false
+                    }
+                }
+                button.physicsBody?.dynamic = false   // Keeps pause play button in place
+                
+                // Updates the value of the variable 'stopped'
+                if (stopped) {
+                    stopped = false
+                } else {
+                    stopped = true
+                }
+            }
+        }
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    override func didSimulatePhysics() {
+        self.enumerateChildNodesWithName("ball", usingBlock: { (node: SKNode!, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+            if node.position.y < 0 {
+                node.removeFromParent()
+            }
+        })
     }
 }
