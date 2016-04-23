3a54d1662a2d29f9d71c53bbf8590388de401f92
yosvanilopez2 <yosvanilopez@nat-oitwireless-inside-vapornet100-a-11478.Princeton.EDU>
2016-04-23 16:06:15 -0400

    fixes

--- Main Project/PhysWiz/PhysWiz/GameScene.swift
+++ Main Project/PhysWiz/PhysWiz/GameScene.swift
@@ -490,7 +490,6 @@ class GameScene: SKScene {
                         let img = String(objectType).lowercaseString + ".png"
                         let newObj = self.createObject(location, image: img)
                         self.addChild(newObj)
-                        viewController.objectflag = 9
                         //selectedShape = newObj
                         //self.addChild(self.createObject(location, image: img))
                     }
@@ -553,7 +552,7 @@ class GameScene: SKScene {
                 // Updates the value of the variable 'stopped'
                 if (stopped) {
                     // being used to try and figure put the time component
-                  var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "runtime", userInfo: nil, repeats: true)
+                  //var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "runtime", userInfo: nil, repeats: true)
                     stopped = false
                     button.texture = SKTexture(imageNamed: "pause.png")
                 } else {
