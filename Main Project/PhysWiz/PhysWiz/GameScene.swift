5388949250c5e0ff8db2e1f2338682e121e32a70
yosvanilopez2 <yosvanilopez@nat-oitwireless-inside-vapornet100-a-11478.Princeton.EDU>
2016-04-23 15:52:45 -0400

    merge stuff

--- Main Project/PhysWiz/PhysWiz/GameScene.swift
+++ Main Project/PhysWiz/PhysWiz/GameScene.swift
@@ -440,10 +440,7 @@ class GameScene: SKScene {
         self.physicsWorld.addJoint(rodJoint2)
     }
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> origin/master
+
     func createRamp(location:CGPoint){
         let newObj = self.createObject(location, image: "ramp.png")
         newObj.size = CGSize(width: 200, height: 200)
