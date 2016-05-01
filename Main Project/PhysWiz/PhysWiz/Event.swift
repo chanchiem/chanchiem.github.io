dfff962e03e807917277850ffad2e0bd39667b1e
jhlin2337 <James@nat-oitwireless-inside-vapornet100-c-31293.Princeton.EDU>
2016-05-01 16:41:02 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit c234e7a12658eb2ca1f874039e99abba20746709.

--- Main Project/PhysWiz/PhysWiz/Event.swift
+++ Main Project/PhysWiz/PhysWiz/Event.swift
@@ -17,10 +17,11 @@
 import Foundation
 
 class Event: NSObject {
-    private var isCollision = false;
-    private var isTime      = false;
-    private var isPos       = false;
-    private var alive       = true;  // Is the event still being checked?
+    private var isCollision                     = false;
+    private var isTime                          = false;
+    private var isPos                           = false;
+    private var alive                           = true;  // Is the event still being checked?
+    private var eventorganizer: EventOrganizer
     
     ////////////////////////////////////////////////////////////
     //////////////// Collision Variables ///////////////////////
@@ -31,11 +32,13 @@ class Event: NSObject {
     ////////////////////////////////////////////////////////////
     //////////////// Time Variables ////////////////////////////
     ////////////////////////////////////////////////////////////
-    private var time: CGFloat! = nil;
+    private var time: CGFloat!  = nil;
+    private var timer: NSTimer! = nil;
     
     
     // Has the event already been executed?
     func hasHappened() -> Bool { return !self.alive; }
+    func setHappened() { self.alive = false; }
     
     ////////////////////////////////////////////////////////////
     //////////////// Collision Parameters //////////////////////
@@ -53,34 +56,81 @@ class Event: NSObject {
         self.sprite2 = sprite2;
     }
     
+    func isCollisionEvent() -> Bool { return self.isCollision }
+    
+    // This function is called by the Contact Delegate to check if
+    // collision matches with the event. It is called pretty constantly.
+    func checkAndTriggerCollision(sprite1: PWObject, sprite2: PWObject)
+    {
+        if (!self.isCollisionEvent()) { return; }
+        
+        let eventSprites = self.getSprites();
+        if (eventSprites?.count != 2) { return; }
+        
+        
+        if (!(eventSprites?.contains(sprite1))!) { return; }
+        if (!(eventSprites?.contains(sprite2))!) { return; }
+        
+        // Collision matches with event!
+        self.setHappened();
+        eventorganizer.triggerEvent(self);
+    }
+    
     
     ////////////////////////////////////////////////////////////
     //////////////// Time Parameters ///////////////////////////
     ////////////////////////////////////////////////////////////
     
-    func compTime(event1: Event, event2:Event) -> Bool {
-        return (event1.getTime() > event2.getTime());
-    }
+    func isTimerEvent() -> Bool { return self.isTime }
 
-    func getTime() -> CGFloat? {
+    // Gets the upper bound of the
+    func getMaxTime() -> CGFloat? {
         if (!isTime) { return nil; }
         return time;
     }
-    func setTime(time: CGFloat) { self.time = time; }
+    
+    func setTime(time: CGFloat) {
+        if (!isTime) { return }
+        self.time = time;
+    }
+    
+    func startTimer() {
+        if (!isTime) { return }
+        if (self.timer == nil) { return }
+        self.timer.fire();
+    }
+    
+    func pauseTimer() {
+        if (!isTime) { return }
+        if (self.timer == nil) { return };
+    }
+    
+    func initTimer(time: CGFloat) {
+        self.setTime(time);
+        let selector_func = #selector(self.triggerTimer)
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(Double(time), target: self, selector: selector_func, userInfo: nil, repeats: false);
+    }
+    
+    func triggerTimer() {
+        NSLog("Timer triggered!");
+        self.setHappened();
+        eventorganizer.triggerEvent(self);
+    }
     
     
     ////////////////////////////////////////////////////////////
     
     
-    private init(isCollision: Bool, isTime: Bool, isPos: Bool) {
-        self.isCollision = isCollision;
-        self.isTime      = isTime;
-        self.isPos       = isPos;
+    private init(isCollision: Bool, isTime: Bool, isPos: Bool, eo: EventOrganizer) {
+        self.isCollision    = isCollision;
+        self.isTime         = isTime;
+        self.isPos          = isPos;
+        self.eventorganizer = eo;
     }
     
-    static func createCollision(sprite1: PWObject, sprite2: PWObject) -> Event? {
+    static func createCollision(eo:EventOrganizer, sprite1: PWObject, sprite2: PWObject) -> Event? {
         if (!PWObject.isPWObject(sprite1) && !PWObject.isPWObject(sprite2)) { return nil; }
-        let event = Event.init(isCollision: true, isTime: false, isPos: false)
+        let event = Event.init(isCollision: true, isTime: false, isPos: false, eo: eo)
     
         event.sprite1 = sprite1;
         event.sprite2 = sprite2;
@@ -88,12 +138,12 @@ class Event: NSObject {
         return event;
     }
     
-    static func createTime(sprite: PWObject, time: CGFloat) -> Event? {
+    static func createTime(eo:EventOrganizer, sprite: PWObject, time: CGFloat) -> Event? {
         if (!PWObject.isPWObject(sprite)) { return nil; }
-        let event = Event.init(isCollision: false, isTime: true, isPos: false)
+        let event = Event.init(isCollision: false, isTime: true, isPos: false, eo: eo)
         
         event.sprite1 = sprite;
-        event.time = time;
+        event.initTimer(time);
         
         return event;
     }
